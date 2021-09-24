86a9eecfd12d413a6b254d3b1e6f9c2a70cf2307
avollkopf <43980694+avollkopf@users.noreply.github.com>
2021-09-24 15:19:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='cbpi4-PIDHerms',
       version='0.0.1',
       description='CraftBeerPi Plugin',
@@ -12,4 +18,9 @@ setup(name='cbpi4-PIDHerms',
       '': ['*.txt', '*.rst', '*.yaml'],
       'cbpi4-PIDHerms': ['*','*.txt', '*.rst', '*.yaml']},
       packages=['cbpi4-PIDHerms'],
+	        install_requires=[
+            'cbpi>=4.0.0.35',
+      ],
+	  long_description=long_description,
+	  long_description_content_type='text/markdown'
      )
